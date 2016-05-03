4082098e84f7e6f31bc641f7bd4ee83ae1147242
tachymetre <vince.pham.149@gmail.com>
2016-05-03 15:49:26 -0400

    update service calls to handle multiple symbols at once

--- app/scripts/services/getStockInfo.js
+++ app/scripts/services/getStockInfo.js
@@ -36,15 +36,26 @@
                 return promise;
             },
             getVisualizedData: function(requestParams) {
-                var startDateStock = "'" + new Date(new Date().setMonth(new Date().getMonth() - 12)).toJSON().slice(0, 10) + "'",
-                    endDateStock = "'" + new Date().toJSON().slice(0, 10) + "'",
-                    prependQueryStartDate = ' and startDate %3D ',
-                    prependQueryEndDate = ' and endDate %3D ',
-                    baseStockUrl = 'https://query.yahooapis.com/v1/public/yql?q=select * from yahoo.finance.historicaldata where symbol %3D ',
-                    appendQueryStockUrl = '&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
+                var startDateStock = new Date(new Date().setMonth(new Date().getMonth() - 12)).toJSON().slice(0, 10),
+                    endDateStock = new Date().toJSON().slice(0, 10),
+                    prependQueryStartDate = ')%20and%20startDate%20%3D%20%22',
+                    prependQueryEndDate = '%22%20and%20endDate%20%3D%20%22',
+                    baseStockUrl = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20in%20(',
+                    appendQueryStockUrl = '%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
 
                 // Check if param(s) is singular or not
-                paramQuery = "'" + requestParams + "'";
+                if (requestParams.indexOf(',') > 0) {
+                    rawParams = requestParams.replace(/\s+/, "").split(',');
+                    rawParams.forEach(function(value, index) {
+                        transformParams = '%22' + value + '%22';
+                        transformArray.push(transformParams);
+                    });
+                    // Make sure to clear the array for next iteration
+                    paramQuery = transformArray.join();
+                    transformArray.length = 0;
+                } else {
+                    paramQuery = '%22' + requestParams + '%22';
+                }
 
                 // Return promise object for async operation in the controller
                 var promise = $http({
